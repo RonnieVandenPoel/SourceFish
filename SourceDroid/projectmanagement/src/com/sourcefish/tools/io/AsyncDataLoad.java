cc4d12d647c89573151ca1d3a3c525a1628be8f5
timartesis <tim.goeyvaerts@student.artesis.be>
2013-03-19 13:26:46 +0100

    dataload

---
+++ SourceDroid/projectmanagement/src/com/sourcefish/tools/io/AsyncDataLoad.java
@@ -0,0 +1,15 @@
+package com.sourcefish.tools.io;
+
+import android.os.AsyncTask;
+
+public class AsyncDataLoad extends AsyncTask<String, Integer, Boolean> {
+
+	@Override
+	protected Boolean doInBackground(String... params) {
+		
+		
+		
+		return null;
+	}
+
+}
