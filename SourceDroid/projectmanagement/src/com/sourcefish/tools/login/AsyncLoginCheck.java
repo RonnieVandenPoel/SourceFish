994b482ffae279c798b2e76f2991db919a9057f5
ronnievandenpoel <Ronnie_vandenpoel@hotmail.com>
2013-03-19 15:31:11 +0100

    useles stuffs

--- SourceDroid/projectmanagement/src/com/sourcefish/tools/login/AsyncLoginCheck.java
+++ SourceDroid/projectmanagement/src/com/sourcefish/tools/login/AsyncLoginCheck.java
@@ -3,17 +3,10 @@ package com.sourcefish.tools.login;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.apache.http.HttpResponse;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.Credentials;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.auth.params.AuthPNames;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.params.AuthPolicy;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONException;
 import org.json.JSONObject;
