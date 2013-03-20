31e38d3ccc9412ca32b6cc59cbe53efa47f28de7
maartenderaedemaeker <maartenderaedemaeker6@gmail.com>
2013-03-20 11:11:21 +0100

    updatez

--- SourceDroid/projectmanagement/src/com/sourcefish/tools/login/RegisterActivity.java
+++ SourceDroid/projectmanagement/src/com/sourcefish/tools/login/RegisterActivity.java
@@ -1,7 +1,31 @@
 package com.sourcefish.tools.login;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import com.actionbarsherlock.app.SherlockActivity;
+import com.sourcefish.projectmanagement.R;
+import com.sourcefish.tools.SourceFishConfig;
 
 public class RegisterActivity extends SherlockActivity {
+	@Override
+	protected void onCreate(Bundle bundle) {
+		super.onCreate(bundle);
+		this.setContentView(R.layout.activity_register);
+		setTheme(SourceFishConfig.MAINTHEME);
+	}
+	
+	public void Register()
+	{
+		TextView emailField=(TextView) findViewById(R.id.uc_txt_registerUsername);
+		TextView passwordField=(TextView) findViewById(R.id.uc_txt_registerPassword);
+		TextView passwordRepeatField=(TextView) findViewById(R.id.uc_txt_registerPasswordrepeat);
+		
+		if(passwordField.getText().equals(passwordRepeatField.getText()))
+		{
+			//register
+		}
+	}
+	
 	
 }
